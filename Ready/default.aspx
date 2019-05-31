3f96fdba5ea3f3bdeade5bbd8b905db021c6bbbe
Jeff Sanders <jsanders@microsoft.com>
2019-05-31 16:40:08 -0400

    Initial

---
+++ Ready/default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Ready._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head runat="server">
+     <meta charset=utf-8>
+    <meta http-equiv=X-UA-Compatible content="IE=edge,chrome=1">
+    <title>MSReady Sample using Sticky Footer &mdash; Solved by Flexbox &mdash; Cleaner, hack-free CSS</title>
+    <meta name=HandheldFriendly content=True>
+    <meta name=MobileOptimized content=320>
+    <meta name=viewport content="width=device-width,minimum-scale=1,maximum-scale=1">
+    <meta name=google-site-verification content=q5KncOje-dNdD5uxqSOG1znSAo5Wsz5MlBcAmteQAJA>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel=stylesheet>
+    <link href=https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css rel=stylesheet>
+    <link href=/css/main.css rel=stylesheet type=text/css> 
+        </head> <body class=Site>
+
+            <header class=Site-header>
+            <div class="Header Header--cozy" role=banner>
+                <div class=Header-titles>
+                    <asp:Label class=Header-title ID="headertitle" runat="server" Text=""></asp:Label>
+                    </br>
+                    <asp:Label class=Header-subTitle  ID="headersubtitle" runat ="server" Text=""></asp:Label>
+                </div>
+        </header>
+        <main class=Site-content>
+            <div class=Container>
+                <h1>List of things</h1>
+                <div class="Demo Demo--spaced">
+                    <asp:Label ID="description" runat="server" Text=""></asp:Label>
+                    
+                </div>
+                <form id="form1" runat="server">
+            <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" /><br />
+            <asp:ListBox runat="server" ID ="ListBox1" > </asp:ListBox><br />
+                <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Refesh" />
+                </br>
+            <label>Hitting Web Server: </label><asp:Label ID="WebSiteName" runat="server" Text="WebSiteName"></asp:Label>
+        </div>
+
+        <asp:Label  ID="message" runat="server"></asp:Label>
+    </form>
+            </div>
+        </main>
+        <footer class=Site-footer>
+            <div class=Footer>
+                <div class=Footer-credits><span class=Footer-credit>See my Blog <a
+                            href="http://jsandersblog.azurewebsites.net" target="_blank">Jeff Sanders</a>. </span> </div>
+            </div>
+        </footer>
+        <script>!function () { var e = document.body.style; if (!("flexBasis" in e || "msFlexAlign" in e || "webkitBoxDirection" in e)) { var o = document.createElement("div"); o.className = "Error", o.innerHTML = "Your browser does not support Flexbox.  Parts of this site may not appear as expected.", document.body.insertBefore(o, document.body.firstChild) } }()</script>
+      
+    
+</body>
+</html>
---
+++ Ready/default.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Ready
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        private string m_ROconnectionString = "";
+        private string m_RWconnectionString = "";
+        private string m_WebsiteName = "";
+        private string m_Title = "";
+        private string m_SubTitle = "";
+        private string m_Description = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Always store connection strings securely. 
+            m_ROconnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ReadOnlyDbConn"].ConnectionString;
+            m_RWconnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["WriteDbConn"].ConnectionString;
+            m_WebsiteName = System.Configuration.ConfigurationManager.AppSettings["WEBSITE_SITE_NAME"];
+            m_Title = System.Configuration.ConfigurationManager.AppSettings["Title"];
+            m_SubTitle = System.Configuration.ConfigurationManager.AppSettings["SubTitle"];
+            m_Description = System.Configuration.ConfigurationManager.AppSettings["Description"];
+            WebSiteName.Text = m_WebsiteName;
+            headertitle.Text = m_Title;
+            headersubtitle.Text = m_SubTitle;
+            description.Text = m_Description;
+
+            // Best practice is to scope the SqlConnection to a "using" block
+            string errorStr = "";
+
+            if (!RefreshListBox(ref errorStr))
+            {
+                message.Text = errorStr;
+            }
+            else
+            {
+                message.Text = "";
+            }
+
+
+        }
+
+        protected bool RefreshListBox(ref string returnVal)
+        {
+            bool abSuccess = true;
+            try
+            {
+                ListBox1.Items.Clear();
+                returnVal = "";
+                
+                // Best practice is to scope the SqlConnection to a "using" block
+                using (SqlConnection conn = new SqlConnection(m_ROconnectionString))
+                {
+                    // Connect to the database
+                    conn.Open();
+
+                    // Read rows
+                    SqlCommand selectCommand = new SqlCommand("SELECT * FROM dbo.ItemTable", conn);
+                    SqlDataReader results = selectCommand.ExecuteReader();
+
+                    // Enumerate over the rows
+                    while (results.Read())
+                    {
+                        ListBox1.Items.Add(String.Format(" ID: {0}, Name: {1}", results[0].ToString(), results[1].ToString()));
+                    }
+                }
+                int count = ListBox1.Items.Count;
+                if (count > 12)
+                {
+                    ListBox1.Rows = 12;
+                }
+                else
+                {
+                    ListBox1.Rows = ListBox1.Items.Count;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                abSuccess = false;
+                returnVal = ex.Message;
+            }
+
+
+
+
+            return abSuccess;
+        }
+
+        protected bool InsertItem(ref string returnVal)
+        {
+            bool abSuccess = true;
+            try
+            {
+                returnVal = "";
+                string valueToInsert = TextBox1.Text;
+               // Best practice is to scope the SqlConnection to a "using" block
+                using (SqlConnection conn = new SqlConnection(m_RWconnectionString))
+                {
+                    // Connect to the database
+                    conn.Open();
+
+                    // Read rows
+                    SqlCommand selectCommand = new SqlCommand(String.Format("insert into dbo.ItemTable (Name) values ('{0}')", valueToInsert), conn);
+                    SqlDataReader results = selectCommand.ExecuteReader();
+                    abSuccess = RefreshListBox(ref returnVal);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                abSuccess = false;
+                returnVal = ex.Message;
+            }
+
+
+
+
+            return abSuccess;
+        }
+
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string err = "";
+            if (TextBox1.Text.Length > 0)
+            {
+                if (!InsertItem(ref err))
+                {
+                    message.Text = err;
+                }
+                else
+                {
+                    message.Text = "inserted";
+                }
+            }
+            else
+            {
+                message.Text = "Enter some text!";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string ret = "";
+            if (!RefreshListBox(ref ret))
+                {
+                message.Text = ret;
+            }
+        }
+    }
+}
